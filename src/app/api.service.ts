6a5b968b1054a8334ed65949a08c7626541d1c4a
Alberto <alberto.elvira@campusfp.es>
2020-10-30 14:41:43 +0100

    Primer Comint

---
+++ src/app/api.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Gallery } from './gallery';
+import { catchError } from 'rxjs/operators';
+
+const apiUrl = 'http://192.168.34.60:3000/gallery';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+  
+  getGalleryById(id: string): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get<Gallery>(url).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  addGallery(gallery: Gallery, file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('imageTitle', gallery.imageTitle);
+    formData.append('imageDesc', gallery.imageDesc);
+    const header = new HttpHeaders();
+    const params = new HttpParams();
+
+    const options = {
+      params,
+      reportProgress: true,
+      headers: header
+    };
+    const req = new HttpRequest('POST', apiUrl, formData, options);
+    return this.http.request(req);
+  }
+  
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GalleryDetailsComponent } from './gallery-details/gallery-details.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path: 'gallery',
+  component: GalleryComponent,
+  data: { title: 'List of Sales' }
+},
+{
+  path: 'gallery-details/:id',
+  component: GalleryDetailsComponent,
+  data: { title: 'Sales Details' }
+},
+{ path: '',
+  redirectTo: '/gallery',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
